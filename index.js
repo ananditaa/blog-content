ac24baee55959baf05e0426231939daed823134f
Ananditaa <81854479+ananditaa@users.noreply.github.com>
2022-02-20 10:59:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,6 @@ function scrolling(){
     var srcHeight = document.documentElement.scrollHeight   -document.documentElement.clientHeight;
     var scrolled = (winScroll/srcHeight)*100;
     document.querySelector('.progress-bar') .style.width=scrolled+'%';
-
-   
     var value=scrolled;
-    var coinAnimation= document.getElementById('coin-id');
     console.log(value);
-    if (value>=50) {
-       coinAnimation.classList.add('coin-animation');
-    }
-    else{
-        coinAnimation.classList.remove('coin-animation');
-    }
-   
-   
 }
